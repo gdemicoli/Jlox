338c87383ef2bc9d32a5c47d339d42ea60f74136
Gustavo <s3898300@student.rmit.edu.au>
2025-06-24 16:26:56 +1000

    Initial interpreter Framework

---
+++ com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,30 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+
+  private static void runFile(String path) {
+    System.out.println("runFile() called with: " + path);
+  }
+
+  private static void runPrompt() {
+    System.out.println("runPrompt() called");
+  }
+}
