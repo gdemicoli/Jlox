3bb17a7cbedc71c9ea21d354c92437527845be5f
Gustavo <s3898300@student.rmit.edu.au>
2025-09-16 12:00:07 +1000

    return statements no parseable

--- src/com/craftinginterpreters/lox/Stmt.java
+++ src/com/craftinginterpreters/lox/Stmt.java
@@ -9,6 +9,7 @@ abstract class Stmt{
    R visitFunctionStmt(Function stmt);
    R visitIfStmt(If stmt);
    R visitPrintStmt(Print stmt);
+   R visitReturnStmt(Return stmt);
    R visitVarStmt(Var stmt);
    R visitWhileStmt(While stmt);
    R visitBreakStmt(Break stmt);
@@ -79,6 +80,20 @@ abstract class Stmt{
    @Override
    <R> R accept(Visitor<R> visitor) {
     return visitor.visitPrintStmt(this);
+   }
+ }
+ static class Return extends Stmt {
+   Return(Token keyword, Expr value) {
+     this.keyword = keyword;
+     this.value = value;
+   }
+
+   final Token keyword;
+   final Expr value;
+
+   @Override
+   <R> R accept(Visitor<R> visitor) {
+    return visitor.visitReturnStmt(this);
    }
  }
  static class Var extends Stmt {
