d508d596501216df17e24c271e748a74f55210c2
Gustavo <s3898300@student.rmit.edu.au>
2025-09-16 11:14:38 +1000

    First implementation of LoxCallable for functions

--- src/com/craftinginterpreters/lox/loxCallable.java
+++ src/com/craftinginterpreters/lox/loxCallable.java
@@ -2,7 +2,7 @@ package com.craftinginterpreters.lox;
 
 import java.util.List;
 
-interface loxCallable {
+interface LoxCallable {
     int arity();
 
     Object call(Interpreter interpreter, List<Object> arguements);
