bbaf10ec07c7d233b366a3bf0ffbdaa32014f8aa
Gustavo <s3898300@student.rmit.edu.au>
2025-09-17 16:43:39 +1000

    added the closure data structure to hold environment of outer function call environments. Now functions defined, then returned within functions work. See testCode.lox for example

--- src/com/craftinginterpreters/lox/LoxFunction.java
+++ src/com/craftinginterpreters/lox/LoxFunction.java
@@ -4,15 +4,17 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
+    private final Environment closure;
 
-    LoxFunction(Stmt.Function declaration) {
+    LoxFunction(Stmt.Function declaration, Environment closure) {
+        this.closure = closure;
         this.declaration = declaration;
     }
 
     @Override
     public Object call(Interpreter interpreter,
             List<Object> arguements) {
-        Environment environment = new Environment(interpreter.globals);
+        Environment environment = new Environment(closure);
 
         for (int i = 0; i < declaration.params.size(); i++) {
             environment.define(declaration.params.get(i).lexeme,
@@ -20,8 +22,7 @@ class LoxFunction implements LoxCallable {
         }
         try {
             interpreter.executeBlock(declaration.body, environment);
-        }
-        catch (Return returnValue) {
+        } catch (Return returnValue) {
             return returnValue.value;
         }
         return null;
