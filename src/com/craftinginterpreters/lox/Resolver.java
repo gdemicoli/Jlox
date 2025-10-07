c7bb625a496bca83e21f3554fbb03bae62354434
Gustavo <s3898300@student.rmit.edu.au>
2025-10-07 15:01:16 +1100

    Started resolver for mainting static scoping

---
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -0,0 +1,22 @@
+package com.craftinginterpreters.lox;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
+    private final Interpreter interpreter;
+
+    Resolver(Interpreter interpreter) {
+        this.interpreter = interpreter;
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        beginScope();
+        resolve(stmt.statements);
+        endScope();
+        return null;
+    }
+}
