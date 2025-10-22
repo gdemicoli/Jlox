805495cfa8b190e01fd259cfb4d41ac03f5aad95
Gustavo <s3898300@student.rmit.edu.au>
2025-10-22 17:59:20 +1100

    Added error checking

--- src/com/craftinginterpreters/lox/Environment.java
+++ src/com/craftinginterpreters/lox/Environment.java
@@ -71,4 +71,8 @@ public class Environment {
         return ancestor(distance).values.get(name);
     }
 
+    void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
+    }
+
 }
